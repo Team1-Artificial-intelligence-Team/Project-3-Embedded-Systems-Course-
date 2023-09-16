d9b47adb85622e199a30292209a04f87684d85a0
Team 1 - Artificial intelligence Team - <143822498+Team1-Artificial-intelligence-Team@users.noreply.github.com>
2023-09-16 10:27:16 +0300

    Add files via upload

---
+++ Source Code/main.c
@@ -0,0 +1,45 @@
+#include "functions.h"
+
+int main()
+{
+    Logo();
+    strcntr("", 100);
+    printf("\n");
+    strcntr("Enter students number:", 100);
+    printf("\n");
+    strcntr("", 100);
+    printf("\n");
+    scanf("%d", &size);
+    do
+    {
+
+        Logo();
+        MainChoice();
+
+        if (choice == 1)
+        {
+            FirstChoice();
+        }
+
+        else if (scan == 0)
+        {
+            printf("You must add students at first!!\n");
+        }
+
+        else if (scan != 0)
+        {
+            if (choice == 2)
+            {
+                SecondChoice();
+            }
+
+            if (choice == 3)
+            {
+                ThirdChoice();
+            }
+        }
+
+    } while (choice != 4);
+
+    return 0;
+}
